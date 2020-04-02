5d3183a850369762bfd41f757b78419e59920299
Christian Burland <christian.burland@vaimo.com>
2020-04-02 21:33:43 +0100

    Remove service worker, update text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import './styles/styles.scss';
 import Root from './root';
-import * as serviceWorker from './service-worker';
 
 render(
   <React.StrictMode>
@@ -10,8 +9,3 @@ render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
